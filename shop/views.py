196841100e230daf487d148299c7f6944a6d49f0
Keshav Kumar <akeshav53@gmail.com>
2019-06-02 00:45:44 +0530

    apps startinng

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django import HttpResponse
+from django.http import HttpResponse
 def index(request):
 	return HttpResponse("Index Shop")
 # Create your views here.
