0ca458991389dfa1e825d3f3ce42fc927bcb8e5d
Keshav Kumar <akeshav53@gmail.com>
2019-06-02 01:45:20 +0530

    urls update

--- shop/views.py
+++ shop/views.py
@@ -2,4 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
 	return render(request,"shop/index.html")
+def about(request):
+	return HttpResponse("We are at about")
+def contact(request):
+	return HttpResponse("We are at contact")
+def productview(request):
+	return HttpResponse("We are at productview")
+def checkout(request):
+	return HttpResponse("We are at checkout")
+def search(request):
+	return HttpResponse("We are at search")
+def tracker(request):
+	return HttpResponse("We are at tracker")
+
 # Create your views here.
