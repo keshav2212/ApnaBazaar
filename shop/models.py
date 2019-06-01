19c13f318f1f3d6d1eb7d44d9859dfdcefccc532
Keshav Kumar <akeshav53@gmail.com>
2019-06-02 02:32:59 +0530

    product table added

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class product(models.Model):
+	product_id=models.AutoField
+	product_name=models.CharField(max_length=50)
+	desc=models.CharField(max_length=100)
+	pub_date=models.DateField()
 
 # Create your models here.
