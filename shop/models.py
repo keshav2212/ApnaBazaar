33eb4c536afb65e67c0d070e72c4bbf4daeb317c
Keshav Kumar <akeshav53@gmail.com>
2019-06-02 16:34:13 +0530

    Adding images and change models

--- shop/models.py
+++ shop/models.py
@@ -2,7 +2,14 @@ from django.db import models
 class product(models.Model):
 	product_id=models.AutoField
 	product_name=models.CharField(max_length=50)
+	category=models.CharField(max_length=50,default="")
+	subcategory=models.CharField(max_length=50,default="")
+	price=models.IntegerField(default=0)
 	desc=models.CharField(max_length=100)
 	pub_date=models.DateField()
+	image=models.ImageField(upload_to="shop/images",default="")
 
+
+	def __str__(self):
+		return self.product_name
 # Create your models here.
