f7ce24ae7248d124e8757be407b28e00da6f988d
Keshav Kumar <akeshav53@gmail.com>
2019-06-04 23:37:07 +0530

    some of products added

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import product
+from math import ceil
 def index(request):
-	return render(request,"shop/index.html")
+	products=product.objects.all()
+	n=len(products)
+	nslides=(n//4)+(ceil(n/4)-n//4)
+	param={'no_of_slides':nslides,'range':range(1,nslides),'product':products}
+	return render(request,"shop/index.html",param)
 def about(request):
 	return render(request,"shop/about.html")
 def contact(request):
