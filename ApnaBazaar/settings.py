15ac694365cf62b37149894d72bb1f7c8c61f400
Keshav Kumar <akeshav53@gmail.com>
2021-07-06 00:49:39 +0530

    cache removed for sometime

--- ApnaBazaar/settings.py
+++ ApnaBazaar/settings.py
@@ -58,18 +58,18 @@ TEMPLATES = [
     },
 ]
 
-CACHE_TTL = 60 * 1500
+# CACHE_TTL = 60 * 1500
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS":{
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
-        },
-        "KEY_PREFIX":"example"
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS":{
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient"
+#         },
+#         "KEY_PREFIX":"example"
+#     }
+# }
 
 WSGI_APPLICATION = 'ApnaBazaar.wsgi.application'
 
--- shop/views.py
+++ shop/views.py
@@ -2,29 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import product
 from math import ceil
-from django.core.cache.backends.base import DEFAULT_TIMEOUT
-from django.views.decorators.cache import cache_page
-from django.core.cache import cache
+# from django.core.cache.backends.base import DEFAULT_TIMEOUT
+# from django.views.decorators.cache import cache_page
+# from django.core.cache import cache
 def index(request):
 	filter = request.GET.get('product',None)
 	context = {}
 	if filter is not None:
-		if cache.get(filter):
-			print("Come from Cache.")
-			context['filter'] = cache.get('filter')
-		else:
-			print("Come from DB.")
-			products=product.objects.filter(product_name__contains = filter)
-			cache.set(filter, products)
-			context['filter'] = filter
+		# if cache.get(filter):
+		# 	print("Come from Cache.")
+		# 	context['filter'] = cache.get('filter')
+		# else:
+		products=product.objects.filter(product_name__contains = filter)
+		cache.set(filter, products)
+		context['filter'] = filter
 	else:
-		if cache.get("all"):
-			print("came from cache")
-			products = cache.get("all")
-		else:
-			print("came from db")
-			products=product.objects.all()
-			cache.set("all", products)
+		# if cache.get("all"):
+		# 	print("came from cache")
+		# 	products = cache.get("all")
+		# else:
+		products=product.objects.all()
+		cache.set("all", products)
 	context['product'] = products
 	return render(request,"shop/index.html",context)
 def about(request):
